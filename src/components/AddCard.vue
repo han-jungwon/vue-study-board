3bd67701316b84ec7911c377a7f7e2e4a98e35fa
han-jungwon <rhehf1024@naver.com>
2022-01-24 01:52:24 +0900

    카드 생성1-마크업까지

---
+++ src/components/AddCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="add-card">
+    <form>
+      <input class="form-control" type="text" v-model="inputTitle" ref="inputText">
+      <button class="btn btn-success" type="submit" :disabled="invalidInput">Add Card</button>
+      <a class="cancel-add-btn" href="" @click.prevent="$emit('close')">&times;</a>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      inputTitle: ''
+    }
+  },
+computed: {
+  invalidInput() {
+    return !this.inputTitle.trim()
+  }
+},
+  mounted () {
+    this.$refs.inputText.focus()
+  }
+}
+
+</script>
